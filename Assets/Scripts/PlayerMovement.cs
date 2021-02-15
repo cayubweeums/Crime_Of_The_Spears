d7d54961482e14953b493c4886c63ec25ce1e429
Caleb <farmingtoncardinals24@gmail.com>
2021-02-15 11:12:18 -0600

    Added Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,38 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed;
     private Rigidbody2D rb2d;
-    private Vector3 velocity; 
+    private float horizontal;
+    private float vertical;
+    private float moveLimiter = 0.7f;
+    public float runSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
-
         rb2d = GetComponent<Rigidbody2D>();
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity = Vector3.zero;
-        velocity.x = Input.GetAxisRaw("Horizontal");
-        velocity.y = Input.GetAxisRaw("Vertical");
-        if(velocity != Vector3.zero)
-        {
-            MoveCharacter();
-        }
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
     }
 
-
-    void MoveCharacter()
+    private void FixedUpdate()
     {
-        rb2d.MovePosition(
-            transform.position + velocity * speed * Time.deltaTime);
+        if(horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        rb2d.velocity = new Vector3(horizontal * runSpeed, vertical * runSpeed);
     }
-
-
 }
